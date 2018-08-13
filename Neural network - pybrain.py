17d1c3ce5f64254c17b4239e5b3137430551f56b
Eduardo <hdefavari@gmail.com>
2018-08-13 15:23:59 -0300

    Add files via upload

---
+++ Neural network - pybrain.py
@@ -0,0 +1,28 @@
+# Rede Neural com PyBrain
+
+from pybrain.datasets        import SupervisedDataSet
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.supervised      import BackpropTrainer
+
+# passa as dimensões dos vetores de entrada e saída
+dataset = SupervisedDataSet(2, 1)
+
+dataset.addSample([1,1], [0])
+dataset.addSample([1,0], [1])
+dataset.addSample([0,1], [1])
+dataset.addSample([0,0], [0])
+
+network = buildNetwork(dataset.indim, 4, dataset.outdim, bias=True)
+trainer = BackpropTrainer(network, dataset, learningrate=0.01, momentum=0.99)
+
+# for epoch in range(1000):
+#     trainer.train()
+
+trainer.trainEpochs(1000)
+
+test_data = SupervisedDataSet(2, 1)
+test_data.addSample([1,1], [0])
+test_data.addSample([1,0], [1])
+test_data.addSample([0,1], [1])
+test_data.addSample([0,0], [0])
+trainer.testOnData(test_data, verbose=True)
